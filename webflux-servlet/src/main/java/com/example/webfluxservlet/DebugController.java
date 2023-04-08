115f2a2f3d1fe7ef1af81a45cbb99019d5ff6864
长安 <18846439952@163.com>
2023-04-08 16:36:29 +0800

    update reactor demo

---
+++ webflux-servlet/src/main/java/com/example/webfluxservlet/DebugController.java
@@ -0,0 +1,22 @@
+package com.example.webfluxservlet;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+/**
+ * @Auther: 长安
+ */
+@RestController
+public class DebugController {
+
+    @GetMapping("/debug")
+    public String debug() {
+        return " debug ";
+    }
+
+    @GetMapping("/mono")
+    public Mono<String> mono() {
+        return Mono.just("single mono!");
+    }
+}
